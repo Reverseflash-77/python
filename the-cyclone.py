2ae13caddef53560d334aa1a0b038f013660f77d
Reverse Flash <reverseflash429@gmail.com>
2026-07-25 17:40:47 +0530

    Add the-cyclone.py solution

---
+++ the-cyclone.py
@@ -0,0 +1,11 @@
+height=int(input("Enter the height of the person in cm: "))
+credits=int(input("Enter the number of credits the person has: "))
+
+if height>=137 and credits>=10:
+    print("Enjoy the ride!")
+elif height<137 and credits>=10:
+    print("You are not tall enough to ride.")
+elif height>=137 and credits<10:
+    print("You do not have enough credits.")
+else:
+    print("You have not met either requirement.")
